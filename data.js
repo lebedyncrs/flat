2173336531cb81e7e5c08b8b96d31b42af7433ff
siroga <serhii@tourradar.com>
2017-09-24 19:54:00 +0200

    add spot

--- data.js
+++ data.js
@@ -24,6 +24,40 @@ var APP_DATA = {
         "pitch": 0.13276995544562453,
         "fov": 1.000890955726551
       },
+      "linkHotspots": [
+        {
+          "yaw": 2.091488619141595,
+          "pitch": 0.2516969611286317,
+          "rotation": 0,
+          "target": "1-kor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kor",
+      "name": "kor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
       "linkHotspots": [],
       "infoHotspots": []
     }
