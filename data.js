4ebd400094af326b66013119cc5b6865ac39e135
siroga <serhii@tourradar.com>
2017-09-27 21:18:55 +0200

    rearange photos

---
+++ data.js
@@ -0,0 +1,207 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bedroom-center",
+      "name": "bedroom-center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -2.824045831424826,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6176553175133233,
+          "pitch": -0.006823707892246844,
+          "rotation": 0,
+          "target": "2-bedroom-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bedroom-corner",
+      "name": "bedroom-corner",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bedroom-corridor",
+      "name": "bedroom-corridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.939994755794892,
+          "pitch": 0.07745177478968657,
+          "rotation": 0,
+          "target": "3-bathroom"
+        },
+        {
+          "yaw": -1.3028440138332602,
+          "pitch": -0.02836343524532481,
+          "rotation": 0,
+          "target": "4-orridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bathroom",
+      "name": "bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.134601502713579,
+          "pitch": -0.2634757960380334,
+          "rotation": 0,
+          "target": "2-bedroom-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-orridor",
+      "name": "сorridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": 0.22046343548572978,
+        "pitch": 0.004147318416201529,
+        "fov": 1.9844389650541938
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14924490380239597,
+          "pitch": 0.1845592862647596,
+          "rotation": 0,
+          "target": "2-bedroom-corridor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.825977625096913,
+          "pitch": 0.35772652065675814,
+          "title": "Exit",
+          "text": "Exit"
+        }
+      ]
+    }
+  ],
+  "name": "Apartment #1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
